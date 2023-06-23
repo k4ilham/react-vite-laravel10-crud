a52dfd839a8c698f6b8f6dc8df28649df330b911
Ilham Maulana <k4ilham@gmail.com>
2023-06-23 11:10:38 +0700

    menampilkan data

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 Tutorial React dan Laravel 10 #1: Cara Install dan Menjalankan React (Vite)
 
+https://www.santrikoding.com/tutorial-react-dan-laravel-10-3-menampilkan-data-di-react-dari-rest-api
+
 npm create vite@4.2.0 react-crud -- --template react
 
 npm install
@@ -7,3 +9,5 @@ npm install
 npm run dev
 
 npm install react-router-dom@6.4.5
+
+npm install axios@1.3.4
--- src/views/posts/index.jsx
+++ src/views/posts/index.jsx
@@ -1,11 +1,81 @@
+//import useState dan useEffect
+import { useState, useEffect } from 'react';
+
+//import api
+import api from '../../api';
+
+//import Link
+import { Link } from 'react-router-dom';
+
 export default function PostIndex() {
+
+    //ini state
+    const [posts, setPosts] = useState([]);
+
+    //define method
+    const fetchDataPosts = async () => {
+
+        //fetch data from API with Axios
+        await api.get('/api/posts')
+            .then(response => {
+                
+                //assign response data to state "posts"
+                setPosts(response.data.data.data);
+            })
+        
+    }
+
+    //run hook useEffect
+    useEffect(() => {
+        
+        //call method "fetchDataPosts"
+        fetchDataPosts();
+
+    }, []);
+
     return (
         <div className="container mt-5 mb-5">
             <div className="row">
                 <div className="col-md-12">
+                    <Link to="/posts/create" className="btn btn-md btn-success rounded shadow border-0 mb-3">ADD NEW POST</Link>
                     <div className="card border-0 rounded shadow">
                         <div className="card-body">
-                            HALAMAN POSTS INDEX
+                            <table className="table table-bordered">
+                                <thead className="bg-dark text-white">
+                                    <tr>
+                                        <th scope="col">Image</th>
+                                        <th scope="col">Title</th>
+                                        <th scope="col">Content</th>
+                                        <th scope="col" style={{ 'width': '15%' }}>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        posts.length > 0
+                                            ?   posts.map((post, index) => (
+                                                    <tr key={ index }>
+                                                        <td className='text-center'>
+                                                            <img src={post.image} alt={post.title} width="200" className='rounded' />
+                                                        </td>
+                                                        <td>{ post.title }</td>
+                                                        <td>{ post.content }</td>
+                                                        <td className="text-center">
+                                                            <Link to={`/posts/edit/${post.id}`} className="btn btn-sm btn-primary rounded-sm shadow border-0 me-2">EDIT</Link>
+                                                            <button className="btn btn-sm btn-danger rounded-sm shadow border-0">DELETE</button>
+                                                        </td>
+                                                    </tr>
+                                                ))
+
+                                            :   <tr>
+                                                    <td colSpan="4" className="text-center">
+                                                        <div className="alert alert-danger mb-0">
+                                                            Data Belum Tersedia!
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                    }
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
